89eae750dc4dff7deeac6d32ce92b5c7b1e31ce7
Zirp302 <vkkakantakt@gmail.com>
2025-05-06 19:28:46 +0500

    Delete test_site directory

--- test_site/pril_test_site/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from pril_test_site.models import Post
-
-admin.site.register(Post)
-# Register your models here.
--- test_site/pril_test_site/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PrilTestSiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pril_test_site'
